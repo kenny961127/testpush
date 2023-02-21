407240fa03b634c2313cdf86b6ba59d4b4ba7ae5
kenny <kenny@redin.tech>
2023-02-21 16:54:13 +0800

    Added TestPush

---
+++ testpush/App.tsx
@@ -0,0 +1,23 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  View,
+} from 'react-native';
+
+
+
+function App(): JSX.Element {
+
+  return (
+    <View style={{flex:1, backgroundColor:'red'}}></View>
+  );
+}
+
+
+export default App;
